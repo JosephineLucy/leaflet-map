a9e1bd2ccf0d271f83ab67493d7e0372b6312740
Josephine Lindley <Josephine.Lindley@MacBook-Pro-2.local>
2023-02-01 17:09:55 +0000

    adds map with some co-ords plotted

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import { Icon } from "leaflet";
 
 function App() {
+  const markerArr = [
+    { name: "TGI Fridays", coOrds: [52.56105422973633, -1.8219709396362305] },
+    {
+      name: "Wyndley Leisure Centre",
+      coOrds: [52.562618255615234, -1.8336868286132812],
+    },
+    {
+      name: "Under Pressure Espresso",
+      coOrds: [52.5593537, -1.8260082],
+    },
+    {
+      name: "Heart of the Country",
+      coOrds: [52.64982986450195, -1.8083280324935913],
+    },
+    {
+      name: "Sunnybank",
+      coOrds: [52.5415476, -1.8340355],
+    },
+    {
+      name: "St Nicholas Church",
+      coOrds: [52.5328563, -1.7391058],
+    },
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="leaflet-container">
+        <MapContainer center={[52.593719, -1.79051]} zoom={12}>
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          />
+          {markerArr.map((marker) => (
+            <Marker
+              key={marker.name}
+              position={[marker.coOrds[0], marker.coOrds[1]]}
+            />
+          ))}
+        </MapContainer>
+      </div>
     </div>
   );
 }
